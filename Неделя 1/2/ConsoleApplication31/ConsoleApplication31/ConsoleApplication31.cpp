7835c28d9d0f1f4b27cc966aa9ddf9d331905ba6
Renceex <71285567+Renceex@users.noreply.github.com>
2022-06-23 10:48:31 +0300

    Add files via upload

---
+++ Неделя 1/2/ConsoleApplication31/ConsoleApplication31/ConsoleApplication31.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+//2h
+
+int main()
+{
+    int a, b, c, d, h ;
+
+    std::cout << "Enter a, b, c, d, h :";
+    std::cin >> a >> b >> c >> d >> h  ;
+
+    std::cout << "S = " << 0.5 * h * (a+b) << std::endl;
+
+    std::cout << "P = " << a + b + c + d << std::endl;
+
+    return 0;
+}
---
+++ Неделя 1/3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+//6
+
+int main()
+{
+    int a, b, c;
+
+    std::cout << "Enter a, b c:\n";
+    std::cin >> a >> b >> c;
+
+    std::cout << "1) " << (a + b + c) / 3 << std::endl;
+
+    std::cout << "2) " << 2 * (a + c) - 3 * b << std::endl;
+
+    return 0;
+}
