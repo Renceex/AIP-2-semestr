154a615f609e01ada4317c9ec84bea8a2fb8e4ed
Renceex <71285567+Renceex@users.noreply.github.com>
2022-06-26 16:37:46 +0300

    Add files via upload

---
+++ Неделя 4,5/2/ConsoleApplication7.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+//1j
+
+int main()
+{
+    int x;
+
+    std::cout << "X = ";
+    std::cin >> x;
+
+    if (x > 3)
+        std::cout << "y = " << 2 * x - 10 << std::endl;
+    else if (x < 3)
+        std::cout << "y = " << pow( 2 * x + 1,2) - 1 << std::endl;
+    else std::cout << "y = 10" << std::endl;
+
+    return 0;
+}
---
+++ Неделя 4,5/3/ConsoleApplication7.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+//1a
+
+int main()
+{
+    int x;
+
+    std::cout << "X = ";
+    std::cin >> x;
+
+    if (x > 0)
+        std::cout << "y = " << 2 * x - 10 << std::endl;
+    else if (x < 0)
+        std::cout << "y = " <<  2 * abs(x) - 1 << std::endl;
+    else std::cout << "y = 0" << std::endl;
+
+    return 0;
+}
