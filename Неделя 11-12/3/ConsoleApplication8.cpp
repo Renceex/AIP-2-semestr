02e445d91552301d34adc6d166e94a47707acaed
Renceex <71285567+Renceex@users.noreply.github.com>
2022-06-27 13:44:08 +0300

    Add files via upload

---
+++ Неделя 11-12/3/ConsoleApplication8.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+//3    
+
+struct robot
+{
+	float dimensions;
+	float weight;
+	float maximumSpeed;
+};
+
+int main()
+{
+	srand(0);
+
+	robot R[5];
+
+	for (int i = 0; i < 5; i++)
+	{
+		R[i].dimensions = rand() % 51 + 50;
+		R[i].weight = rand() % 101 + 50;
+		R[i].maximumSpeed = rand() % 41 + 10;
+	}
+
+	robot TheLittle = R[4];
+
+	for (int i = 1; i < 5; i++)
+		if (R[i].dimensions < TheLittle.dimensions)
+			TheLittle = R[i];
+
+	std::cout << "dimensions: " << TheLittle.dimensions
+		<< "\nweight: " << TheLittle.weight
+		<< "\nmaximumSpeed: " << TheLittle.maximumSpeed << std::endl;
+
+	return 0;
+}
