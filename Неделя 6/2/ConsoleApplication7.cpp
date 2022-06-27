b2b89d3991ae592e5127728e8e7a61f023c222f6
Renceex <71285567+Renceex@users.noreply.github.com>
2022-06-27 12:03:56 +0300

    Add files via upload

---
+++ Неделя 6/2/ConsoleApplication7.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+//2З
+
+int main()
+{
+    float x, y;
+
+    std::cout << "X = ";
+    std::cin >> x;
+
+    std::cout << "Y = ";
+    std::cin >> y;
+
+    float R = sqrt(pow(x, 2) + pow(y, 2)) >= 1;
+
+    if (x <= -y - 1 && y <= 1 && R)
+        std::cout << "Hit";
+    else
+        std::cout << "missed";
+
+    return 0;
+}
---
+++ Неделя 6/3/ConsoleApplication7.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+//3Б
+
+int main()
+{
+    float x, y;
+
+    std::cout << "X = ";
+    std::cin >> x;
+
+    std::cout << "Y = ";
+    std::cin >> y;
+
+    float R = sqrt(pow(x, 2) + pow(y, 2)) >= 1;
+
+    if ( y <= x/2 && x<= 2*y && R)
+        std::cout << "Hit";
+    else
+        std::cout << "missed";
+
+    return 0;
+}
